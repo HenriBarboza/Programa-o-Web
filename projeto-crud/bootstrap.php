dff7013d16999aa80cde62aa3146d0ca04546778
HenriBarboza <henr.fe7@gmail.com>
2024-04-13 11:03:27 -0300

    Início do projeto crud

---
+++ projeto-crud/bootstrap.php
@@ -0,0 +1,36 @@
+<?php
+
+require __DIR__ ."/vendor/autoload.php";
+
+
+$metodo = $_SERVER['REQUEST_METHOD'];
+$caminho = $_SERVER['PATH_INFO'] ?? '/';
+
+$r = new Php\ProjetoBanco\Router($metodo, $caminho);
+
+## Rotas
+
+$r->get('/', 'Php\ProjetoBanco\Controllers\HomeController@index');
+
+
+
+
+## Fim das rotas
+
+$resultado = $r->handler();
+
+if(!$resultado){
+    http_response_code(404);
+    echo "Página não encontrada!";
+    die();
+}
+
+if ($resultado instanceof Closure){
+    echo $resultado($r->getParams());
+} elseif (is_string($resultado)){
+    $resultado = explode("@", $resultado);
+    $controller = new $resultado[0];
+    $resultado = $resultado[1];
+    echo $controller->$resultado($r->getParams());
+}
+
---
+++ projeto-crud/src/Views/Home/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Home</title>
+</head>
+
+<body>
+    <div class="container mt-4">
+        <h1>Projeto 2 - MVC</h1>
+        <div>
+            <h2>Eleitores</h2>
+        <a href="eleitores" class="btn btn-primary me-3 mt-3">Vizualizar</a>
+            <h2>Municipio</h2>
+        <a href="eleitores" class="btn btn-primary me-3 mt-3">Vizualizar</a>
+            <h2>Cadidato</h2>
+        <a href="eleitores" class="btn btn-primary me-3 mt-3">Vizualizar</a>
+            <h2>Voto</h2>
+        <a href="eleitores" class="btn btn-primary me-3 mt-3">Vizualizar</a>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</body> 
+
+</html>
