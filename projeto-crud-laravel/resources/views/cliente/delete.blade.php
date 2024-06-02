d3614b3b73aeb624e9a98e150101def4d0a978fd
Henri Barboza <henr.fe7@gmail.com>
2024-06-02 17:34:56 -0300

    Novo projeto em laravel

---
+++ projeto-crud-laravel/resources/views/cliente/delete.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário de Exclusão de Clientes</title>
+</head>
+
+<body>
+    <h1>Formulário de Exclusão de Clientes</h1>
+    <form action="{{route('clientes.destoy')}}">
+        @CSRF
+        @metod('DELETE')
+        <label for="nome">Informe o nome do cliente</label>
+        <input type="text" name="nome" id="nome"><br>
+        <label for="telefone">Informe o telefone do cliente</label>
+        <input type="text" name="telefone" id="telefone"><br>
+        <label for="email">Informe o e-mail do cliente</label>
+        <input type="text" name="email" id="email"><br>
+        <button type="submit">Salvar</button>
+    </form>
+</body>
+
+</html>
